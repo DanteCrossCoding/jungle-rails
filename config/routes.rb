dc0ee276cf85e29b80423eb97ef755ed1fa07ab5
Dante Cross <dantecross@outlook.com>
2021-01-13 06:31:06 +0000

    added user signup/login function and password protection

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     post   :remove_item
   end
 
-  get '/about' => 'about#show'
+  
 
   resources :orders, only: [:create, :show]
 
@@ -19,6 +19,15 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
+
+  get '/about' => 'about#show'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
